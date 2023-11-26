73cdde639cdee441634519a010549ea2487486b1
Michael-Williams49 <147909653+Michael-Williams49@users.noreply.github.com>
2023-11-27 00:03:37 +0800

    Update background.js

--- background.js
+++ background.js
@@ -4,7 +4,7 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
       target: { tabId: sender.tab.id },
       func: async () => {
         const source = document.documentElement.outerHTML;
-        const matches = source.match(/isSlideShow\("\/(.*?)\"\)/g);
+        const matches = source.match(/isSlideShow\("\/(.*?)\"\)/gi);
         if (!matches) {
           console.error("No slideshows found on this page");
           return;
@@ -14,7 +14,27 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
           const url = match.match(/"\/(.*?)"/)[1];
           const downloadUrl = `http://180.76.151.202/${url}`;
           console.log('Download URL:', downloadUrl);
-          window.location = downloadUrl
+          window.open(downloadUrl, '_blank');
+        }
+      }
+    });
+  }
+  if (message.action === 'downloadVideos') {
+    chrome.scripting.executeScript({
+      target: { tabId: sender.tab.id },
+      func: async () => {
+        const source = document.documentElement.outerHTML;
+        const matches = source.match(/<video[^>]*src="(.*?)"[^>]*>/gi);
+        if (!matches) {
+          console.error("No videos found on this page");
+          return;
+        }
+        
+        for (const match of matches) {
+          const url = match.match(/(?<=")[^"]*(?=")/g)[2];
+          const downloadUrl = url;
+          console.log('Download URL:', downloadUrl);
+          window.open(downloadUrl, '_blank');
         }
       }
     });
